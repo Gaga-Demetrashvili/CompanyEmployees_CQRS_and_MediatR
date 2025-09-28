b9fd715cff8c761cccf10a86e653b481759dd123
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-09-28 14:53:21 +0400

    Add project files.

---
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Infrastructure.Presentation.Controllers;
+
+[Route("api/companies")]
+[ApiController]
+public class CompaniesController : ControllerBase
+{
+    public CompaniesController() { }
+
+    
+}
